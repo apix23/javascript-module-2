be3eb14f5d470df64099910e93fe7e98474d3afc
Alejandro Sanchez <24274685+apix23@users.noreply.github.com>
2022-10-25 17:53:44 +0200

    modulo completado

--- week-2/Homework/Part-2/D-methods/exercise-2.js
+++ week-2/Homework/Part-2/D-methods/exercise-2.js
@@ -5,13 +5,13 @@ Once you fix them, run this file, it should output the correct values!
 */
 
 let person = {
-    name: "Alice",
-    age: 25,
-    currentAddress: "Glasgow",
-    changeAddress: (newAddress) {
-        currentAddress = newAddress;
-    },
-    celebrateBirthday: function {
-        that.age = that.age + 1;
-    }
+  name: "Alice",
+  age: 25,
+  currentAddress: "Glasgow",
+  changeAddress: function (newAddress) {
+    currentAddress = newAddress;
+  },
+  celebrateBirthday: function () {
+    this.age = this.age + 1;
+  },
 };
